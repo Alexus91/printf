48e464f97ec99d8a2f1774e8faf90f7e61858100
Bckforgood <116983963+Bckforgood@users.noreply.github.com>
2023-04-24 23:31:22 +0000

    Update base_functions.c

--- base_functions.c
+++ base_functions.c
@@ -23,29 +23,29 @@ unsigned char flags, int wid, int prec)
 {
 	int size;
 	char digit, pad = '0';
-	unsigned int ret = 1;
+	unsigned int r = 1;
 
 	for (size = 0; *(base + size);)
 		size++;
 
 	if (num >= size || num <= -size)
-		ret += convert_sbase(output, num / size, base, flags, wid - 1, prec - 1);
+		r += convert_sbase(output, num / size, base, flags, wid - 1, prec - 1);
 	else
 	{
 		for (; prec > 1; prec--, wid--) /* Handle precision */
-		ret += _memcpy(output, &pad, 1);
+		r += _memcpy(output, &pad, 1);
 
 		if (NEG_FLAG == 0) /* Handle width */
 		{
 			pad = (ZERO_FLAG == 1) ? '0' : ' ';
 			for (; wid > 1; wid--)
-			ret += _memcpy(output, &pad, 1);
+			r += _memcpy(output, &pad, 1);
 			}
 		}
 
 		digit = base[(num < 0 ? -1 : 1) * (num % size)];
 		_memcpy(output, &digit, 1);
-	return (ret);
+	return (r);
 }
 
 /**
@@ -63,13 +63,13 @@ unsigned char flags, int wid, int prec)
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 unsigned char flags, int wid, int prec)
 {
-	unsigned int size, ret = 1;
+	unsigned int size, r = 1;
 	char digit, pad = '0', *lead = "0x";
 
 	for (size = 0; *(base + size);)
 		size++;
 	if (num >= size)
-		ret += convert_ubase(output, num / size, base, flags, wid - 1, prec - 1);
+		r += convert_ubase(output, num / size, base, flags, wid - 1, prec - 1);
 	else
 	{
 		if (((flags >> 5) & 1) == 1) /* Printing a ptr address */
@@ -77,16 +77,16 @@ unsigned char flags, int wid, int prec)
 			wid -= 2;
 			prec -= 2; }
 		for (; prec > 1; prec--, wid--) /* Handle precision */
-			ret += _memcpy(output, &pad, 1);
+			r += _memcpy(output, &pad, 1);
 		if (NEG_FLAG == 0) /* Handle width */
 		{
 			pad = (ZERO_FLAG == 1) ? '0' : ' ';
 			for (; wid > 1; wid--)
-				ret += _memcpy(output, &pad, 1); }
+				r += _memcpy(output, &pad, 1); }
 
 		if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
-			ret += _memcpy(output, lead, 2); }
+			r += _memcpy(output, lead, 2); }
 	digit = base[(num % size)];
 	_memcpy(output, &digit, 1);
-return (ret);
+return (r);
 }
